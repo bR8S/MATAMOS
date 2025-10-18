9aa55f66e026f7f19917b739a874f3131153cad5
guillermo mata <guillermomata@macbookpro.attlocal.net>
2025-10-18 12:52:52 -0700

    feat: added new animations on load and on open details

--- loadAnime.js
+++ loadAnime.js
@@ -34,17 +34,17 @@
     
     load_tl.to(logo, {duration: 0.75, top: 0, opacity: 1, ease: "power2.out", delay: 0.5}, "+=0.25")
         .to(stars, {duration: 0.75, stagger: 0.15, opacity: 1, ease: "power2.out"}, "-=0.25")
-        .to(projectSelectors, {duration: 0.15, stagger: 0.125, opacity: 1, ease: "power2.out"}, "-=0.5")
-        .to(projectDivider, {duration: 0.65, width: '100%', opacity: 1, ease: "power2.out"}, "-=0.1")
+        .to(projectSelectors, {duration: 0.15, stagger: 0.125, opacity: 1, ease: "power2.out"}, "-=1")
+        .to(projectDivider, {duration: 0.65, width: '100%', opacity: 1, ease: "power2.out"}, "-=0.5")
         .to(projectsContainer, {duration: 0.15, opacity: 1, ease: "power2.out"}, "-=0.1")
         .to(workProjects, {duration: 0.35, stagger: 0.15, opacity: 1, ease: "power2.out"}, "+=0.1")
 
     gsap.to(overlayHorizontalLines, {duration: 1, width: '100%', stagger: 0.25, ease: "power2.inOut"})
     gsap.to(overlayVerticalLines, {duration: 1, height: '100%', stagger: 0.25, ease: "power2.inOut"})
 
-    gsap.to(socialsText, {duration: 0.45, top: 0, stagger: 0.15, opacity: 1, ease: "power2.out", delay: 3})
-    gsap.to(leftHandText, {duration: 0.45, top: 0, stagger: 0.15, opacity: 1, ease: "power2.out", delay: 3})
-    gsap.to(bioLines, {duration: 0.45, top: 0, stagger: 0.15, opacity: 1, ease: "power2.out", delay: 3})
-    gsap.to(bioText, {duration: 0.01, top: 0, opacity: 1, ease: "power2.out", delay: 3})
-    gsap.to(banner, {duration: 0.5, top: 'calc(100% - 56px)', opacity: 1, ease: "power2.out", delay: 3.85}) 
+    gsap.to(socialsText, {duration: 0.45, top: 0, stagger: 0.15, opacity: 1, ease: "power2.out", delay: 2.25})
+    gsap.to(leftHandText, {duration: 0.45, top: 0, stagger: 0.15, opacity: 1, ease: "power2.out", delay: 2.65})
+    gsap.to(bioLines, {duration: 0.45, top: 0, stagger: 0.15, opacity: 1, ease: "power2.out", delay: 2.65})
+    gsap.to(bioText, {duration: 0.01, top: 0, opacity: 1, ease: "power2.out", delay: 2.65})
+    gsap.to(banner, {duration: 0.5, top: 'calc(100% - 56px)', opacity: 1, ease: "power2.out", delay: 3.5}) 
  });
